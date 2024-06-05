7356c935ef350837c86b191170d5b9453939893e
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2024-06-05 12:57:22 +0300

    update resolvers, add context to apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,41 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import fs from 'fs';
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+import Query from "./resolvers/Query";
+import { Mutation } from "./resolvers/Mutation";
+import { PrismaClient } from "@prisma/client";
+import { Context } from "./types";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const prisma = new PrismaClient();
 
 /**
  * Define resolvers
  */
 const resolvers = {
-  Query: {
-    info: () => `This is the API of Midnight PC Store!`
-  }
-}
+  Query,
+  Mutation,
+};
 
 /**
  * Initialize Apollo server
  */
 const server = new ApolloServer({
-  typeDefs: fs.readFileSync(
-    path.join(__dirname, './schema.graphql'),
-    'utf8'
-  ),
+  typeDefs: fs.readFileSync(path.join(__dirname, "./schema.graphql"), "utf8"),
   resolvers,
-})
+});
 
 /**
  * Start Apollo server
  */
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
+  context: async (): Promise<Context> => ({
+    prisma,
+  }),
 });
 
 console.log(`🚀  Server ready at: ${url}`);
