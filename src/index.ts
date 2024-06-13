baba7fb6192e8b9e98960bb6964f4dd47c259b53
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2024-06-13 16:33:07 +0300

    update import

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,13 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import path from "path";
 import { fileURLToPath } from "url";
 import fs from "fs";
-import Query from "./resolvers/Query";
 import { PrismaClient } from "@prisma/client";
 import { Context } from "./types";
 import Product from "./resolvers/Product";
 import Image from "./resolvers/Image";
 import { Mutation } from "./resolvers/Mutation";
 import Category from "./resolvers/Category";
+import { Query } from "./resolvers/Query";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
