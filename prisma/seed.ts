f5b3e71758f09772a99edaed31aa68437d37b06e
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2024-06-13 15:58:20 +0300

    remove unused import

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Prisma } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { userData } from "./seed-data/userData";
 import { productData } from "./seed-data/productData";
 
