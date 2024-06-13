647fd9bf8134d394148920b3876727c8ee8cc9d9
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2024-06-13 16:35:31 +0300

    make resolvers asynchronous and include export them as methods of Query root type

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -2,63 +2,66 @@ import { Category, Image, Product, User } from "@prisma/client";
 import { Context, QueryProduct } from "src/types";
 
 /**
- * Find and return a product by id
- *
- * @param _parent _parent
- * @param args arguments
- * @param context context
- * @returns product
+ * Define resolvers for Query root type
  */
-function product(__parent: Product, args: QueryProduct, context: Context) {
-  return context.prisma.product.findUnique({ where: { id: args.id } });
-}
+export const Query = {
+  /**
+   * Find and return a product by id
+   *
+   * @param _parent _parent
+   * @param args arguments
+   * @param context context
+   * @returns product
+   */
+  product: async (_parent: Product, args: QueryProduct, context: Context) => {
+    return await context.prisma.product.findUnique({ where: { id: args.id } });
+  },
 
-/**
- * Find and return all products
- *
- * @param _parent _parent
- * @param _args arguments
- * @param context context
- * @returns product
- */
-function products(_parent: Product, _args: unknown, context: Context) {
-  return context.prisma.product.findMany();
-}
+  /**
+   * Find and return all products
+   *
+   * @param _parent _parent
+   * @param _args arguments
+   * @param context context
+   * @returns products
+   */
+  products: async (_parent: Product, _args: unknown, context: Context) => {
+    return await context.prisma.product.findMany();
+  },
 
-/**
- * Find and return all users
- *
- * @param _parent _parent
- * @param _args arguments
- * @param context context
- * @returns users
- */
-function users(_parent: User, _args: unknown, context: Context) {
-  return context.prisma.user.findMany();
-}
+  /**
+   * Find and return all users
+   *
+   * @param _parent _parent
+   * @param _args arguments
+   * @param context context
+   * @returns users
+   */
+  users: async (_parent: User, _args: unknown, context: Context) => {
+    return await context.prisma.user.findMany();
+  },
 
-/**
- * Find and return all images
- *
- * @param _parent _parent
- * @param _args arguments
- * @param context context
- * @returns images
- */
-function images(_parent: Image, _args: unknown, context: Context) {
-  return context.prisma.image.findMany();
-}
+  /**
+   * Find and return all images
+   *
+   * @param _parent _parent
+   * @param _args arguments
+   * @param context context
+   * @returns images
+   */
+  images: async (_parent: Image, _args: unknown, context: Context) => {
+    return await context.prisma.image.findMany();
+  },
 
-/**
- * Find and return all categories
- *
- * @param _parent _parent
- * @param _args arguments
- * @param context context
- * @returns categories
- */
-function categories(_parent: Category, _args: unknown, context: Context) {
-  return context.prisma.category.findMany();
-}
-
-export default { product, products, users, images, categories };
+  /**
+   * Find and return all categories
+   *
+   * @param _parent _parent
+   * @param _args arguments
+   * @param context context
+   * @returns categories
+   */
+  categories: async (_parent: Category, _args: unknown, context: Context) => {
+    return await context.prisma.category.findMany();
+  },
+};
