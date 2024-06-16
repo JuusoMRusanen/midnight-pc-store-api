8dd354e619588738de9847f43dd5f928716c1923
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2024-06-16 17:41:06 +0300

    add more query resolvers

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,5 +1,5 @@
-import { Category, Image, Product, User } from "@prisma/client";
-import { Context, QueryProduct } from "src/types";
+import { Category, Image, Order, Product, User } from "@prisma/client";
+import { Context, QueryOrder, QueryProduct, QueryUser } from "src/types";
 
 /**
  * Define resolvers for Query root type
@@ -30,6 +30,42 @@ export const Query = {
   },
 
   /**
+   * Find and return order
+   *
+   * @param _parent _parent
+   * @param args arguments
+   * @param context context
+   * @returns order
+   */
+  order: async (_parent: Order, args: QueryOrder, context: Context) => {
+    return await context.prisma.order.findUnique({ where: { id: args.id } });
+  },
+
+  /**
+   * Find and return all orders
+   *
+   * @param _parent _parent
+   * @param _args arguments
+   * @param context context
+   * @returns orders
+   */
+  orders: async (_parent: Order, _args: unknown, context: Context) => {
+    return await context.prisma.order.findMany();
+  },
+
+  /**
+   * Find and return all orders
+   *
+   * @param _parent _parent
+   * @param _args arguments
+   * @param context context
+   * @returns orders
+   */
+  orderProducts: async (_parent: Order, _args: unknown, context: Context) => {
+    return await context.prisma.orderProduct.findMany();
+  },
+
+  /**
    * Find and return all users
    *
    * @param _parent _parent
@@ -39,6 +75,18 @@ export const Query = {
    */
   users: async (_parent: User, _args: unknown, context: Context) => {
     return await context.prisma.user.findMany();
+  },
+
+  /**
+   * Find and return user
+   *
+   * @param _parent _parent
+   * @param args arguments
+   * @param context context
+   * @returns user
+   */
+  user: async (_parent: User, args: QueryUser, context: Context) => {
+    return await context.prisma.user.findUnique({ where: { id: args.id } });
   },
 
   /**
